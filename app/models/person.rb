7ed58a66d437f7d1bc540a604ecbb8041407b92c
Mohd Sameer <mohdsameer1310@gmail.com>
2018-09-04 16:49:46 +0530

    minor fixes and added default value of coupon balance

--- app/models/person.rb
+++ app/models/person.rb
@@ -42,7 +42,7 @@
 #  min_days_between_community_updates :integer          default(1)
 #  deleted                            :boolean          default(FALSE)
 #  cloned_from                        :string(22)
-#  coupon_balance_cents               :integer
+#  coupon_balance_cents               :integer          default(0)
 #
 # Indexes
 #
