440804adb3a536c6df737a18854a60a5262b9dd0
Mohd Sameer <mohdsameer1310@gmail.com>
2018-11-15 14:18:26 +0530

    ducat balance to Avon-bucks changes

--- app/controllers/preauthorize_transactions_controller.rb
+++ app/controllers/preauthorize_transactions_controller.rb
@@ -1,5 +1,5 @@
 # coding: utf-8
-class PreauthorizeTransactionsController < ApplicationController
+class PreauthorizeTransactionsController < ApplicationController 
 
   before_action do |controller|
    controller.ensure_logged_in t("layouts.notifications.you_must_log_in_to_do_a_transaction")
@@ -34,7 +34,7 @@ class PreauthorizeTransactionsController < ApplicationController
     if validation_result.success
       initiation_success(validation_result.data)
     else
-      initiation_error(validation_result.data)
+      initiation_error(validation_result.data) 
     end
   end
 
@@ -58,7 +58,7 @@ class PreauthorizeTransactionsController < ApplicationController
       if params[:payment_type].eql?('coupon_pay')
         price_break_down = price_break_down_locals(validation_result.data, listing)
         if price_break_down[:total] > @current_user.coupon_balance
-          error_msg = "Insufficient Ducat balance! Please contact Avontage to learn how you can earn more Ducats."
+          error_msg = "Insufficient  Avon-BUCKS! Please contact Avontage to learn how you can earn more Avon-BUCKS."
           render_error_response(request.xhr?, error_msg, listing_path(listing))
         else
           initiated_success(validation_result.data)
