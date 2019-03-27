c999ac7f60c1e91e781d2aa1fa7047ff8b1e8a14
mohdsameer <mohdsameer1310@gmail.com>
2019-03-27 18:40:34 +0530

    Added tabs and functionalities on transactions screen

--- app/controllers/inboxes_controller.rb
+++ app/controllers/inboxes_controller.rb
@@ -25,14 +25,6 @@ class InboxesController < ApplicationController
         last_activity_ago: time_ago(inbox_row[:last_activity_at]),
         title: inbox_title(inbox_row, inbox_payment(inbox_row))
       )
-
-      if inbox_row[:type] == :transaction
-        extended_inbox.merge(
-          listing_url: listing_path(id: inbox_row[:listing_id])
-        )
-      else
-        extended_inbox
-      end
     }
 
     paginated_inbox_rows = WillPaginate::Collection.create(pagination_opts[:page], pagination_opts[:per_page], count) do |pager|
@@ -54,19 +46,51 @@ class InboxesController < ApplicationController
   end
 
   def transactions
+
     # We use pageless scroll, so the page should be always the first one (1) when request was not AJAX request
     params[:page] = 1 unless request.xhr?
-
     pagination_opts = PaginationViewUtils.parse_pagination_opts(params)
 
     avon_bucks_histories = @current_user.avon_bucks_histories.order('created_at desc').paginate(page: params[:page], per_page: pagination_opts[:per_page])
 
+    inbox_rows = InboxService.inbox_data(@current_user.id, @current_community.id, pagination_opts[:limit], pagination_opts[:offset])
+
+    # Transactions Messages & Management are on aseparated from Inbox messages 
+    inbox_rows = inbox_rows.select{|row| row[:type].eql?(:transaction)} 
+
+    count = inbox_rows.count 
+
+    inbox_rows = inbox_rows.map { |inbox_row|  
+      inbox_row.merge(  
+        path: path_to_conversation_or_transaction(inbox_row), 
+        last_activity_ago: time_ago(inbox_row[:last_activity_at]),  
+        title: inbox_title(inbox_row, inbox_payment(inbox_row)),  
+        listing_url: listing_path(id: inbox_row[:listing_id]) 
+      )
+    } 
+
+    paginated_inbox_rows = WillPaginate::Collection.create(pagination_opts[:page], pagination_opts[:per_page], count) do |pager| 
+      pager.replace(inbox_rows) 
+    end
+
     if request.xhr?
-      render :partial => "transaction_row",
-        :collection => avon_bucks_histories, :as => :history
+      if params[:type].eql?("avon_bucks_history")
+        render :partial => "transaction_row",
+          :collection => avon_bucks_histories, :as => :history
+      else
+        render :partial => "inbox_row",
+          :collection => paginated_inbox_rows, :as => :conversation,
+          locals: {
+            payments_in_use: @current_community.payments_in_use?
+          }
+      end
     else
-      render locals: { history_rows: avon_bucks_histories}
-    end    
+      render locals: { 
+        history_rows: avon_bucks_histories,
+        inbox_rows: paginated_inbox_rows,
+        payments_in_use: @current_community.payments_in_use?
+      }
+    end
   end
 
   private
