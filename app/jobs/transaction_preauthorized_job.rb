caf2c1d6a4a80bbfaadc22091f8af7a71ad6efe9
mohdsameer <mohdsameer1310@gmail.com>
2019-09-05 14:53:38 +0530

    Added SMS functionality to send SMS to the listing author when listing purchased

--- app/jobs/transaction_preauthorized_job.rb
+++ app/jobs/transaction_preauthorized_job.rb
@@ -13,6 +13,10 @@ class TransactionPreauthorizedJob < Struct.new(:transaction_id)
 
   def perform
     transaction = Transaction.find(transaction_id)
+    transaction_url = Rails.application.routes.url_helpers.person_transaction_url(person_id: transaction.author.id, id: transaction.id, :host => Rails.env.eql?("development") ? "lvh.me:3000" : "avontage.com" )
     MailCarrier.deliver_now(TransactionMailer.transaction_preauthorized(transaction))
+    if transaction.author.mobile_number.present?
+      SMSNotification.sms_service(transaction.author.mobile_number, "Hi #{transaction.author.given_name},\n Congratulations! #{transaction.buyer.given_name} just purchased your offer #{transaction.listing.title}. Click here to respond ASAP #{transaction_url}")
+    end
   end
 end
