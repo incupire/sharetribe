7072f185f5789cf8854d160b9b44f59d122c1d20
Mohd Sameer <mohdsameer1310@gmail.com>
2020-06-23 18:28:18 +0530

    search with tags is done

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -441,6 +441,7 @@ class PeopleController < Devise::RegistrationsController
         :twitter_link,
         :image,
         :description,
+        :tags,
         :password,
         :password2,
         :referral_code,
